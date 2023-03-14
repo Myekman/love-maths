bc49985604f4444d7d48cf5ecc307b4fa9db2693
Myekman <my@traningstemplet.se>
2023-03-14 19:34:13 +0000

    add content to runGame function to create random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
-function  runGame() {
+// wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+
+function runGame() {
 
 }
 
@@ -23,10 +42,9 @@ function displayAdditionQuestion() {
 }
 
 function displayubstractQuestion() {
-    
+
 }
 
 function displayultiplyQuestion() {
-    
-}
 
+}
